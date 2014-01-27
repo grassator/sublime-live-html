c97d9049d90fc7298a6d8893bbf54bbc7355a85c
Dmitriy Kubyshkin <dmitriy@kubyshkin.ru>
2014-01-27 14:32:23 +0700

    Got rid of global vars.

--- livehtml.py
+++ livehtml.py
@@ -1,11 +1,6 @@
 import sublime, sublime_plugin
 import urllib.request
 
-# Holds currently loaded views where live HTML is enabled
-enabled_views = []
-port = 55555
-host = '127.0.0.1'
-
 def content(view):
   "Returns content of given view"
   return view.substr(sublime.Region(0, view.size()))
@@ -25,14 +20,13 @@ def toggle_indicator(view, state):
     view.erase_status('live-html-active')
 
 def send_updated_html(view):
-  global port, host
   try:
     details = urllib.parse.urlencode({
       'file': view.file_name(),
       'content': content(view)
     })
     details = details.encode('UTF-8')
-    url = urllib.request.Request('http://' + host + ':' + str(port) + '/changed', details)
+    url = urllib.request.Request('http://' + LiveHtmlListener.host + ':' + str(LiveHtmlListener.port) + '/changed', details)
     stream = urllib.request.urlopen(url)
     stream.read()
     stream.close()
@@ -42,24 +36,26 @@ def send_updated_html(view):
     return False
 
 def check_live_html_server():
-  global port, host
-  url = urllib.request.Request('http://' + host + ':' + str(port) + '/status')
+  url = urllib.request.Request('http://' + LiveHtmlListener.host + ':' + str(LiveHtmlListener.port) + '/status')
   stream = urllib.request.urlopen(url)
   stream.read()
   stream.close()
 
 class LiveHtmlListener(sublime_plugin.EventListener):
+  # Holds currently loaded views where live HTML is enabled
+  enabled_views = []
+  port = 55555
+  host = '127.0.0.1'
+
   def on_close(self, view):
-    global enabled_views, active_view
     try:
-      enabled_views.remove(view.id())
+      LiveHtmlListener.enabled_views.remove(view.id())
     except:
       return
 
   def on_modified_async(self, view):
-    global enabled_views, active_view
     try:
-      enabled_views.index(view.id())
+      LiveHtmlListener.enabled_views.index(view.id())
     except:
       return
     if view.file_name():
@@ -71,16 +67,15 @@ class LiveHtmlListener(sublime_plugin.EventListener):
 
 class ToggleLiveHtmlCommand(sublime_plugin.TextCommand):  
   def run(self, edit):
-    global enabled_views
     if not is_html_view(self.view) and not is_css_view(self.view):
       return
     try:
-      enabled_views.remove(self.view.id())
+      LiveHtmlListener.enabled_views.remove(self.view.id())
       toggle_indicator(self.view, False)
     except:
       try:
         check_live_html_server()
-        enabled_views.append(self.view.id())
+        LiveHtmlListener.enabled_views.append(self.view.id())
         send_updated_html(self.view)
         toggle_indicator(self.view, True)
       except:
